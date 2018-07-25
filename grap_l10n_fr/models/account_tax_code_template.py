f34e302c1f2e47d5fa8d930eb13ea2af329946f7
Quentin Dupont <quentin.dupont@ntymail.com>
2018-07-25 11:41:08 +0200

    [ADD] account tax code for EBP

---
+++ grap_l10n_fr/models/account_tax_code_template.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+# Copyright (C) 2018-Today GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import fields, models
+
+
+class AccountTaxCodeTemplate(models.Model):
+    _inherit = 'account.tax.code.template'
+
+    ref_nb = fields.Char(
+            "Tax Code\'s Account suffix in EBP", size=4,
+            help="""When exporting Entries to EBP, this suffix will be"""
+            """ appended to the Account Number to make it a new Account.""")
