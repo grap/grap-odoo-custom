a1cf79bdc50e9b4434bc1b0ea52166d896e35bc8
Quentin Dupont <quentin.dupont@ntymail.com>
2019-01-24 18:36:25 +0100

    [ADD] module handle ean8 format in product

---
+++ grap_product_handle_ean8/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2017 - Today: GRAP (http://www.grap.coop)
+# @author: Quentin DUPONT (https://twitter.com/pondupont)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Product - Handle EAN8 Barcode format',
+    'version': '8.0.1.0.0',
+    'category': 'Product',
+    'author': 'GRAP',
+    'summary': 'Handle EAN8 Barcode format',
+    'depends': [
+        'sale',
+        'stock',
+        'purchase',
+    ],
+    'installable': True,
+}
