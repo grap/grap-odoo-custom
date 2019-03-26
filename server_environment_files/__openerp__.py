c750b8469d5461fdfe1fc9d323bcceb0922a54da
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-03-26 14:38:12 +0100

    [ADD] server_environment_files

---
+++ server_environment_files/__openerp__.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+# Copyright (C) 2019 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+{
+    'name': 'GRAP - Configuration Environment Files',
+    'version': '8.0.1.0.0',
+    'category': 'GRAP - Custom',
+    'author': 'GRAP',
+    'licence': 'AGPL-3',
+    'depends': [
+        'base',
+        'server_environment',
+    ],
+    'data': [
+        'views/templates.xml',
+    ],
+    'installable': True,
+}
