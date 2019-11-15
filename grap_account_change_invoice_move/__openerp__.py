529365421a87399b5e581a84a084a979bed08e29
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-11-15 13:49:37 +0100

    [REF] grap_account_change_invoice_move: Black python code

--- grap_account_change_invoice_move/__openerp__.py
+++ grap_account_change_invoice_move/__openerp__.py
@@ -4,14 +4,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'GRAP - Custom accounting move generation from invoices',
-    'version': '8.0.2.0.0',
-    'category': 'GRAP Custom',
-    'license': 'AGPL-3',
-    'author': 'GRAP',
-    'website': 'http://www.grap.coop',
-    'depends': [
-        'account',
-    ],
-    'installable': False,
+    "name": "GRAP - Custom accounting move generation from invoices",
+    "version": "8.0.2.0.0",
+    "category": "GRAP Custom",
+    "license": "AGPL-3",
+    "author": "GRAP",
+    "website": "http://www.grap.coop",
+    "depends": ["account"],
+    "installable": False,
 }
--- grap_account_change_invoice_move/models/account_invoice.py
+++ grap_account_change_invoice_move/models/account_invoice.py
@@ -7,7 +7,7 @@ from openerp import models
 
 
 class AccountInvoice(models.Model):
-    _inherit = 'account.invoice'
+    _inherit = "account.invoice"
 
     def inv_line_characteristic_hashcode(self, invoice_line):
         """
@@ -15,7 +15,8 @@ class AccountInvoice(models.Model):
         Lines having the same hashcode will be grouped together if the journal
         has the 'group line' option. """
         return "%s-%s-%s-%s" % (
-            invoice_line['account_id'],
-            invoice_line.get('tax_code_id', 'False'),
-            invoice_line.get('analytic_account_id', 'False'),
-            invoice_line.get('date_maturity', 'False'))
+            invoice_line["account_id"],
+            invoice_line.get("tax_code_id", "False"),
+            invoice_line.get("analytic_account_id", "False"),
+            invoice_line.get("date_maturity", "False"),
+        )
