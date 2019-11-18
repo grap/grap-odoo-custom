9081ea713456fc9e0d962e6de4f0b1fab9838a9a
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-11-18 18:20:52 +0100

    [MOVE] grap_l10n_fr into grap-odoo-custom-account

--- grap_l10n_fr/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import account_account_template
-from . import account_fiscal_position_tax_template
-from . import account_fiscal_position_template
-from . import account_tax_code_template
-from . import account_tax_template
--- grap_l10n_fr/readme/CONTRIBUTORS.rst
+++
@@ -1,2 +0,0 @@
-* Quentin DUPONT <quentin.dupont@grap.coop>
-* Sylvain LE GAL (https://www.twitter.com/legalsylvain)
