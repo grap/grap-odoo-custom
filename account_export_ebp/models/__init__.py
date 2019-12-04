cbccfbf499acba4bb58ddcf82c17bb6f4ef56216
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-12-04 12:28:30 +0100

    [REM] move account_export_ebp into grap-odoo-custom-account repo. (grap_account_export_ebp)

--- account_export_ebp/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-
-from . import account_account
-from . import account_journal
-from . import account_move
-from . import account_tax_code
-from . import ebp_export
-from . import res_company
-from . import res_partner
--- account_export_ebp/wizard/__init__.py
+++
@@ -1,5 +0,0 @@
-# coding: utf-8
-from . import wizard_ebp_export
-from . import wizard_ebp_unexport
-from . import wizard_res_partner_add_suffix
-from . import wizard_res_partner_add_suffix_line
