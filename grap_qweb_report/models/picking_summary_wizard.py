51b8bece75f1bd468f7548969bcc8dfa0dd0d8e8
Quentin Dupont <perso@quentindupont.fr>
2020-01-08 10:40:00 +0100

    First commit - supprimer fichiers inutiles, commenter ce que je porte pas, et étiquette normal large

--- grap_qweb_report/models/picking_summary_wizard.py
+++ grap_qweb_report/models/picking_summary_wizard.py
@@ -1,33 +1,30 @@
-# coding: utf-8
 # Copyright (C) 2015 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, fields, models
+from odoo import api, fields, models
 
 
 class PickingSummaryWizard(models.TransientModel):
-    _name = 'picking.summary.wizard'
+    _name = "picking.summary.wizard"
 
     # Default Section
     def _default_picking_line_qty(self):
-        return len(self._context.get('active_ids', []))
+        return len(self._context.get("active_ids", []))
 
     def _default_picking_line_ids(self):
-        picking_obj = self.env['stock.picking']
+        picking_obj = self.env["stock.picking"]
         res = []
-        picking_ids = self._context.get('active_ids', [])
+        picking_ids = self._context.get("active_ids", [])
         for picking in picking_obj.browse(picking_ids):
-            res.append((0, 0, {
-                'picking_id': picking.id,
-            }))
+            res.append((0, 0, {"picking_id": picking.id,}))
         return res
 
     def _default_product_line_ids(self):
-        picking_obj = self.env['stock.picking']
+        picking_obj = self.env["stock.picking"]
         res = []
         product_lines = {}
-        picking_ids = self._context.get('active_ids', [])
+        picking_ids = self._context.get("active_ids", [])
         for picking in picking_obj.browse(picking_ids):
             for move in picking.move_lines:
                 # TODO FIXME, manage different uom by stock.move
@@ -37,35 +34,47 @@ class PickingSummaryWizard(models.TransientModel):
                     product_lines[move.product_id.id] += move.product_qty
 
         for product_id, quantity_total in product_lines.iteritems():
-            res.append((0, 0, {
-                'product_id': product_id,
-                'quantity_total': quantity_total,
-            }))
+            res.append(
+                (
+                    0,
+                    0,
+                    {
+                        "product_id": product_id,
+                        "quantity_total": quantity_total,
+                    },
+                )
+            )
         return res
 
     # Columns Section
-    print_summary = fields.Boolean(string='Print Summary', default=True)
+    print_summary = fields.Boolean(string="Print Summary", default=True)
 
-    print_detail = fields.Boolean(string='Print Detail', default=True)
+    print_detail = fields.Boolean(string="Print Detail", default=True)
 
     product_line_ids = fields.One2many(
-        comodel_name='picking.summary.wizard.product',
-        inverse_name='wizard_id', default=_default_product_line_ids)
+        comodel_name="picking.summary.wizard.product",
+        inverse_name="wizard_id",
+        default=_default_product_line_ids,
+    )
 
     standard_price_total = fields.Float(
-        compute='_compute_standard_price_total')
+        compute="_compute_standard_price_total"
+    )
 
     picking_line_ids = fields.One2many(
-        comodel_name='picking.summary.wizard.picking',
-        inverse_name='wizard_id', default=_default_picking_line_ids)
+        comodel_name="picking.summary.wizard.picking",
+        inverse_name="wizard_id",
+        default=_default_picking_line_ids,
+    )
 
     picking_line_qty = fields.Integer(
-        string='Delivery Qty', readonly=True,
-        default=_default_picking_line_qty)
+        string="Delivery Qty", readonly=True, default=_default_picking_line_qty
+    )
 
     # Compute Section
     @api.multi
     def _compute_standard_price_total(self):
         self.ensure_one()
         self.standard_price_total = sum(
-            self.mapped('product_line_ids.standard_price_total'))
+            self.mapped("product_line_ids.standard_price_total")
+        )
--- grap_qweb_report/models/sale_order.py
+++ grap_qweb_report/models/sale_order.py
@@ -1,16 +1,16 @@
-# coding: utf-8
 # Copyright (C) 2018 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, models
+from odoo import api, models
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     @api.multi
     def print_quotation(self):
         self.ensure_one()
-        return self.env['report'].get_action(
-            self, 'grap_qweb_report.grap_template_sale_order')
+        return self.env["report"].get_action(
+            self, "grap_qweb_report.grap_template_sale_order"
+        )
