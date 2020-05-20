b83739d7b5a473daf2e765047b284e0e62770fef
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-05-20 19:35:02 +0200

    [MIG] server_environment_files: Migration to 12.0

--- server_environment_files/__manifest__.py
+++ server_environment_files/__manifest__.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 # Copyright (C) 2019 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
@@ -6,11 +5,13 @@
 
 {
     "name": "GRAP - Configuration Environment Files",
-    "version": "8.0.1.0.0",
+    "summary": "Add custom CSS and extra text on PoS ticket"
+    " depending on the environment",
+    "version": "12.0.1.0.0",
     "category": "GRAP - Custom",
     "author": "GRAP",
     "license": "AGPL-3",
-    "depends": ["base", "server_environment", "point_of_sale",],
-    "data": ["views/templates.xml",],
-    "installable": False,
+    "depends": ["server_environment_ir_config_parameter", "pos_environment"],
+    "data": ["views/templates.xml"],
+    "installable": True,
 }
--- server_environment_files/readme/DESCRIPTION.rst
+++ server_environment_files/readme/DESCRIPTION.rst
@@ -5,5 +5,8 @@ It adds two settings, depending on the environment :
 
 * Custom css to clearly distinguish which server you are acting on
 
+.. figure:: ../static/description/login_page.png
+
 * messages on the PoS Bill if the server is a non production server
-  (It uses ``pos_environment`` for that purpose, if the module is installed)
+
+.. figure:: ../static/description/point_of_sale_ticket.png
