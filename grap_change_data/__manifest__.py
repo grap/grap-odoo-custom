126708fed811750480e30d307723136ad6a92b4f
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-05-20 09:46:46 +0200

    [MIG] grap_change_data: Migration to 12.0

--- grap_change_data/__manifest__.py
+++ grap_change_data/__manifest__.py
@@ -1,16 +1,15 @@
-# coding: utf-8
 # Copyright (C) 2015 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "GRAP - Change Data",
-    "version": "8.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "GRAP - Custom",
     "author": "GRAP",
     "website": "http://www.grap.coop",
     "license": "AGPL-3",
     "depends": ["product",],
     "data": ["data/product_category.xml",],
-    "installable": False,
+    "installable": True,
 }
