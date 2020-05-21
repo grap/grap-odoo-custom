9e23a2fdb76f740e764fc305686566519a358bb7
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-05-21 21:32:24 +0200

    [MIG] grap_cooperative: Migration to 12.0

--- grap_cooperative/models/grap_activity_people.py
+++ grap_cooperative/models/grap_activity_people.py
@@ -1,21 +1,15 @@
-# coding: utf-8
 # Copyright (C) 2014 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import _, api, fields, models
+from odoo import fields, models
 
 
 class GrapActivityPeople(models.Model):
     _name = "grap.activity.people"
-    _order = "people_id"
-    _rec_name = "complete_name"
+    _description = "GRAP Activity - People"
 
-    #    def name_get(self, cr, uid, ids, context=None):
-    #        res = []
-    #        for pc in self.browse(cr, uid, ids):
-    #            res.append((pc.id, pc.complete_name))
-    #        return res
+    _order = "people_id"
 
     # Columns section
     activity_id = fields.Many2one(
@@ -33,44 +27,36 @@ class GrapActivityPeople(models.Model):
         ondelete="cascade",
     )
 
-    fte = fields.Float(
-        string="FTE",
-        required=True,
-        digits=(9, 1),
-        default=1,
-        help="Full Time Equilalent",
+    function = fields.Char(string="Function")
+
+    people_name = fields.Char(
+        string="People Name",
+        related="people_id.name",
+        readonly=True,
     )
 
-    working_email = fields.Char(
-        string="Contact EMail",
+    people_image_small = fields.Binary(
+        string="People Small-sized image",
+        related="people_id.image_small"
+    )
+
+    people_working_email = fields.Char(
+        string="Contact Email",
         related="people_id.working_email",
         readonly=True,
     )
 
-    private_phone = fields.Char(
+    people_private_phone = fields.Char(
         string="Private Phone",
         related="people_id.private_phone",
         readonly=True,
     )
 
-    working_phone = fields.Char(
+    people_working_phone = fields.Char(
         string="Working Phone",
         related="people_id.grap_member_id.working_phone",
         readonly=True,
     )
-
-    complete_name = fields.Char(
-        string="Name", compute="_compute_complete_name", store=True
-    )
-
-    # Compute Section
-    @api.multi
-    @api.depends("activity_id.name", "fte")
-    def _compute_complete_name(self):
-        for item in self:
-            item.complete_name = _(
-                "%s (%s FTE" % (item.activity_id.name, item.fte)
-            )
 
     # Constraints section
     _sql_constraints = [
