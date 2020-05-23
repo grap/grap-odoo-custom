a401e3d00fe28b7b3420782ecf61dea6b6737278
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-05-23 23:56:34 +0200

    [REF] grap_change_access: Black python code

--- grap_change_access/models/product_template.py
+++ grap_change_access/models/product_template.py
@@ -9,37 +9,52 @@ from openerp.exceptions import ValidationError
 
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
     # Constraint section
-    @api.constrains('expense_pdt', 'income_pdt')
+    @api.constrains("expense_pdt", "income_pdt")
     def _check_pos_ok_res_group(self):
-        if self.env.user.has_group('account.group_account_manager'):
+        if self.env.user.has_group("account.group_account_manager"):
             return
         if self.filtered(lambda x: x.expense_pdt or x.income_pdt):
-            raise ValidationError(_(
-                "You can not create an POS expense or income product"
-                " if you're not account manager !"))
+            raise ValidationError(
+                _(
+                    "You can not create an POS expense or income product"
+                    " if you're not account manager !"
+                )
+            )
 
-    @api.constrains('expense_pdt', 'income_pdt', 'sale_ok', 'purchase_ok')
+    @api.constrains("expense_pdt", "income_pdt", "sale_ok", "purchase_ok")
     def _check_purchase_sale_pos_ok(self):
         for product in self.filtered(lambda x: x.expense_pdt or x.income_pdt):
-            if (product.sale_ok or product.purchase_ok):
-                raise ValidationError(_(
-                    "You can not create a product that is both salable or"
-                    " purchasable and POS expense or income product !"))
+            if product.sale_ok or product.purchase_ok:
+                raise ValidationError(
+                    _(
+                        "You can not create a product that is both salable or"
+                        " purchasable and POS expense or income product !"
+                    )
+                )
 
     @api.constrains(
-        'expense_pdt', 'income_pdt', 'property_account_expense',
-        'property_account_income')
+        "expense_pdt",
+        "income_pdt",
+        "property_account_expense",
+        "property_account_income",
+    )
     def _check_pos_property_account(self):
         for product in self.filtered(lambda x: x.expense_pdt):
             if not product.property_account_expense:
-                raise ValidationError(_(
-                    "You can not create an POS expense product without account"
-                    " expense !"))
+                raise ValidationError(
+                    _(
+                        "You can not create an POS expense product without account"
+                        " expense !"
+                    )
+                )
         for product in self.filtered(lambda x: x.income_pdt):
             if not product.property_account_income:
-                raise ValidationError(_(
-                    "You can not create an POS income product without account"
-                    " income !"))
+                raise ValidationError(
+                    _(
+                        "You can not create an POS income product without account"
+                        " income !"
+                    )
+                )
