5eded6ea2d6a4303eeb72fabf04a926a57ba858c
Quentin Dupont <perso@quentindupont.fr>
2020-05-27 16:51:35 +0200

    [ADD][12.0] add new module grap_theme

---
+++ grap_theme/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2013-Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# @author Quentin DUPONT (https://twitter.com/pondupont)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "GRAP - Web Theme",
+    "summary": "Customize Odoo web User Interface",
+    "version": "12.0.1.0.1",
+    "category": "GRAP Custom",
+    "author": "GRAP",
+    "website": "http://www.grap.coop",
+    "license": "AGPL-3",
+    "depends": [
+        "web",
+    ],
+    "data": [
+        "views/templates.xml",
+    ],
+    "installable": True,
+}
