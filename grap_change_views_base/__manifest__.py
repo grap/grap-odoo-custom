ffdce98ebcdd7d89755a4dd031a33da3e2362057
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-06-15 12:37:33 +0200

    [ADD] new module grap_change_views_base

---
+++ grap_change_views_base/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright (C) 2013-Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# @author Julien WESTE
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "GRAP - Change Base Views",
+    "version": "12.0.0.0.1",
+    "category": "GRAP - Custom",
+    "author": "GRAP",
+    "website": "http://www.grap.coop",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+        "delivery",
+        "purchase",
+        "sale_invoice_group_method",
+        "stock",
+    ],
+    "data": [
+        "views/view_ir_model_access.xml",
+        "views/view_res_company.xml",
+        "views/view_res_partner.xml",
+    ],
+    "installable": True,
+}
