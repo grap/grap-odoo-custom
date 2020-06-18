79cec116fb624ad302a432d6352e75612adea999
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-06-18 11:21:16 +0200

    [FIX] populate image_small and image_medium

--- grap_cooperative/migrations/12.0.1.0.2/post-migration.py
+++ grap_cooperative/migrations/12.0.1.0.2/post-migration.py
@@ -15,3 +15,8 @@ attachment_fields = {
 @openupgrade.migrate(use_env=True)
 def migrate(env, version):
     openupgrade_90.convert_binary_field_to_attachment(env, attachment_fields)
+
+    # Force to recompute image_medium and small
+    for member in env["grap.member"].search([("id", "!=", 0)]):
+        if member.image:
+            member.write({"image": member.image})
