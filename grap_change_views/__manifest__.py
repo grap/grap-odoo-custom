33836c1a72187a94ea8ca6a7df6e8400503fe604
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-09-05 18:32:42 +0200

    various fixes

--- grap_change_views/__manifest__.py
+++ grap_change_views/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2013-Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # @author Julien WESTE
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'GRAP - Change Views (Obsolete)',
-    'version': '8.0.7.0.0',
+    'version': '12.0.1.0.1',
     'category': 'GRAP - Custom',
     'author': 'GRAP',
     'website': 'http://www.grap.coop',
