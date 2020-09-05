5292da9619a6978373e702be5e972ddc00188e2e
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-09-05 18:25:40 +0200

    [FINISH] make grap_change_views module empty

---
+++ grap_change_views/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# @author Julien WESTE
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'GRAP - Change Views (Obsolete)',
+    'version': '8.0.7.0.0',
+    'category': 'GRAP - Custom',
+    'author': 'GRAP',
+    'website': 'http://www.grap.coop',
+    'license': 'AGPL-3',
+    'depends': [
+        'grap_change_views_account',
+        'grap_change_views_base',
+        'grap_change_views_pos',
+        'grap_change_views_product',
+        'grap_change_views_purchase',
+        'grap_change_views_sale',
+    ],
+    'installable': True,
+}
