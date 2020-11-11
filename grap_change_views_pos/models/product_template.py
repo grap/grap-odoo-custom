4aded0e15ff0984adfe39ebd595cb746f6421640
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-11-11 11:17:37 +0100

    [IMP] grap_change_views_pos : set available_in_pos True by default

---
+++ grap_change_views_pos/models/product_template.py
@@ -0,0 +1,11 @@
+# Copyright (C) 2018-Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    available_in_pos = fields.Boolean(default=True)
