40cda6ac0ef4ac2366823b1481005134b0e98842
Quentin Dupont <perso@quentindupont.fr>
2021-01-28 16:26:21 +0100

    [IMP] New precisions

---
+++ grap_change_precision/models/purchase_order_line.py
@@ -0,0 +1,14 @@
+# Copyright (C) 2020 - Today: GRAP (http://www.grap.coop)
+# @author: Quentin DUPONT
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+from odoo.addons import decimal_precision as dp
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = "purchase.order.line"
+
+    price_unit = fields.Float(
+        digits=dp.get_precision("GRAP Purchase Price Unit")
+    )
