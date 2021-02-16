5d7ad50d7b9ca85359881728aa23871d4f9ab70f
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-02-16 20:23:41 +0100

    [BLACK]

--- grap_theme/controllers/main.py
+++ grap_theme/controllers/main.py
@@ -11,18 +11,18 @@ from odoo.addons.web import controllers
 
 
 class Binary(controllers.main.Binary):
-
-    @http.route([
-        '/web/binary/company_logo',
-        '/logo',
-        '/logo.png',
-    ], type='http', auth="none", cors="*")
+    @http.route(
+        [
+            "/web/binary/company_logo",
+            "/logo",
+            "/logo.png",
+        ],
+        type="http",
+        auth="none",
+        cors="*",
+    )
     def company_logo(self, dbname=None, **kw):
         placeholder = functools.partial(
-            get_resource_path,
-            'grap_theme',
-            'static',
-            'src',
-            'img'
+            get_resource_path, "grap_theme", "static", "src", "img"
         )
         return http.send_file(placeholder("grap_logo.png"))
