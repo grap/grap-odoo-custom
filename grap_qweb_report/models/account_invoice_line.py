5d7ea2de68696c6364b8c7c2f2b6fc1b52542d09
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-02-16 20:23:41 +0100

    [BLACK]

--- grap_qweb_report/models/account_invoice_line.py
+++ grap_qweb_report/models/account_invoice_line.py
@@ -9,13 +9,12 @@ class AccountInvoiceLine(models.Model):
     _inherit = "account.invoice.line"
 
     price_total_displayed = fields.Monetary(
-        string="Amount (w / wo taxes)",
-        compute="_compute_price_total_displayed")
+        string="Amount (w / wo taxes)", compute="_compute_price_total_displayed"
+    )
 
     def _compute_price_total_displayed(self):
         for line in self:
-            price_include = any(line.mapped(
-                "invoice_line_tax_ids.price_include"))
+            price_include = any(line.mapped("invoice_line_tax_ids.price_include"))
             if price_include:
                 line.price_total_displayed = line.price_total
             else:
