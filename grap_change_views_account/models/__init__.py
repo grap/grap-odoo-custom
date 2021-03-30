354c394f2d67898c3eec66def5d26b5c6a725e91
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-03-30 22:07:25 +0200

    [IMP] grap_change_views_account : set default value for res_company.account_subsequence_method

--- grap_change_views_account/models/__init__.py
+++ grap_change_views_account/models/__init__.py
@@ -1,3 +1,4 @@
 from . import account_invoice
 from . import account_move
 from . import account_tax_group
+from . import res_company
---
+++ grap_change_views_account/models/res_company.py
@@ -0,0 +1,12 @@
+# Copyright (C) 2021 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    account_subsequence_method = fields.Selection(
+        default="fiscal_year_setting")
