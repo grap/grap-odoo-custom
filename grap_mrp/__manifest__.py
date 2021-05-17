036096053cc034e2f987207e3a2136acfbe99ab4
Quentin Dupont <perso@quentindupont.fr>
2021-05-17 16:23:12 +0200

    [WIP] Grap MRP

---
+++ grap_mrp/__manifest__.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2021 - Today: GRAP (http://www.grap.coop)
+# @author: Quentin Dupont (quentin.dupont@grap.coop)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "GRAP - MRP Manufacture R&D - Work in Progress",
+    "summary": "Install MRP modules for R&D",
+    "version": "12.0.1.1.1",
+    "category": "GRAP - Custom",
+    "author": "GRAP",
+    "website": "http://www.grap.coop",
+    "license": "AGPL-3",
+    "depends": [
+        "mrp",
+        "sale_mrp",
+        "mrp_bom_cost",
+        "mrp_bom_component_menu",
+        "mrp_auto_assign",
+        "mrp_bom_note",
+        "mrp_bom_tracking",
+        "mrp_production_grouped_by_product",
+        "mrp_production_note",
+        "mrp_warehouse_calendar",
+        "product_mrp_info",
+    ],
+    "installable": True,
+}
