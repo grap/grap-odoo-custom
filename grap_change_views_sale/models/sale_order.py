790b9a81a2d168fcabf49fd0502907aff4a5704c
Quentin Dupont <perso@quentindupont.fr>
2021-05-21 12:10:15 +0200

    [IMP] Button archive

---
+++ grap_change_views_sale/models/sale_order.py
@@ -0,0 +1,12 @@
+# Copyright (C) 2021-Today: GRAP (http://www.grap.coop)
+# @author: Quentin DUPONT (quentin.dupont@grap.coop)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    # Column Section
+    state = fields.Selection(selection_add=[("done", "Archived")])
