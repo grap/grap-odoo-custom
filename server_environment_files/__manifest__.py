b07596af7a81cfc82d2e06424195ee1edac90364
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-10-25 09:55:33 +0200

    [12.0][REF] uniformize website in __manifest__.py + add pre-commit check

--- server_environment_files/__manifest__.py
+++ server_environment_files/__manifest__.py
@@ -8,6 +8,7 @@
     "summary": "Add custom CSS and extra text on PoS ticket"
     " depending on the environment",
     "version": "12.0.1.0.2",
+    "website": "https://github.com/grap/grap-odoo-custom",
     "category": "GRAP - Custom",
     "author": "GRAP",
     "license": "AGPL-3",
