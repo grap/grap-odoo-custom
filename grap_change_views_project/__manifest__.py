6de1281dacddbcf33a6c4c5785db1273f659fcfb
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-02-14 16:32:35 +0100

    [ADD] new module grap_change_views_product

---
+++ grap_change_views_project/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2022-Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "GRAP - Change Project Views",
+    "version": "12.0.1.0.0",
+    "category": "GRAP - Custom",
+    "author": "GRAP",
+    "website": "https://github.com/grap/grap-odoo-custom",
+    "license": "AGPL-3",
+    "depends": [
+        "project",
+    ],
+    "data": [
+        "views/templates.xml",
+    ],
+    "installable": True,
+}
