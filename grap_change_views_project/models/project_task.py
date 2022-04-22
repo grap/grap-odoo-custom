6240cfde1c5e24d77c1442596aae11b18215b27e
Mouna SEBTI <mouna.sebti@grap.coop>
2022-04-22 10:29:42 +0200

    [REM] remove track_visibility

--- grap_change_views_project/models/project_task.py
+++ grap_change_views_project/models/project_task.py
@@ -8,8 +8,6 @@ from odoo import fields, models
 class ProjectTask(models.Model):
     _inherit = "project.task"
 
-    github_link_1 = fields.Char(
-        string="Pull request n°1", track_visibility="onchange"
-    )  # max default size = 250
-    github_link_2 = fields.Char(string="Pull request n°2", track_visibility="onchange")
-    github_link_3 = fields.Char(string="Pull request n°3", track_visibility="onchange")
+    github_link_1 = fields.Char(string="Pull request n°1")
+    github_link_2 = fields.Char(string="Pull request n°2")
+    github_link_3 = fields.Char(string="Pull request n°3")
