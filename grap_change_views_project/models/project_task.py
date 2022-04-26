7ff63b6dc4f29f5309e08aac759bfd7fdf548d35
Mouna SEBTI <mouna.sebti@grap.coop>
2022-04-26 10:50:39 +0200

    [IMP] project.project : move is_odoo_dev // project.task : add modules_to_install, modules_to_uninstall and sql_requests

--- grap_change_views_project/models/project_task.py
+++ grap_change_views_project/models/project_task.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2022-Today: GRAP (http://www.grap.coop)
-# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# @author: Mouna SEBTI (https://github.com/mounasb)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
@@ -11,3 +11,12 @@ class ProjectTask(models.Model):
     github_link_1 = fields.Char(string="Pull request n°1")
     github_link_2 = fields.Char(string="Pull request n°2")
     github_link_3 = fields.Char(string="Pull request n°3")
+
+    is_odoo_dev_project = fields.Boolean(
+        "Odoo Development", related="project_id.is_odoo_development"
+    )
+
+    to_install_modules = fields.Char("Modules to install")
+    to_uninstall_modules = fields.Char("Modules to uninstall")
+    sql_requests = fields.Text("SQL requests")
+    to_install = False
