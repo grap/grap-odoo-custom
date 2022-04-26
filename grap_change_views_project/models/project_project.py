785e4b2c31ff638d013cd9648960c2fd569d2ca0
Mouna SEBTI <mouna.sebti@grap.coop>
2022-04-26 10:50:39 +0200

    [IMP] project.project : move is_odoo_dev // project.task : add modules_to_install, modules_to_uninstall and sql_requests

--- grap_change_views_project/models/project_project.py
+++ grap_change_views_project/models/project_project.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2022-Today: GRAP (http://www.grap.coop)
-# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# @author: Mouna SEBTI (https://github.com/mounasb)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
@@ -9,4 +9,4 @@ class ProjectProject(models.Model):
     _inherit = "project.project"
 
     description = fields.Html(string="Description")
-    is_odoo_development = fields.Boolean(string="Is Odoo Development")
+    is_odoo_development = fields.Boolean(string="Odoo Development")
