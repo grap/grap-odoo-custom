eb38792e400b1e89708c7d3757ba78f7b552b5b2
Quentin Dupont <perso@quentindupont.fr>
2022-05-23 17:20:40 +0200

    technical module for MRP dev

---
+++ grap_mrp_dev/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2021 - Today: GRAP (http://www.grap.coop)
+# @author: Quentin Dupont (quentin.dupont@grap.coop)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "GRAP - MRP technical module for development",
+    "summary": "Install all MRP modules for Grap",
+    "version": "12.0.1.0.0",
+    "category": "GRAP - Custom",
+    "author": "GRAP",
+    "website": "https://github.com/grap/grap-odoo-custom",
+    "license": "AGPL-3",
+    "depends": [
+        "mrp",
+        "purchase",
+        "sale_mrp",
+        "mrp_bom_note",
+        # GRAP
+        "grap_change_views_mrp",
+        "mrp_food",
+    ],
+    "installable": True,
+}
