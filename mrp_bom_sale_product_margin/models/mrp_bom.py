d98076eb96b4fd0e1dc5c8e6931afe523d79d4ee
Quentin Dupont <perso@quentindupont.fr>
2022-07-28 17:22:00 +0200

    [IMP] Bom Sale product margin : tests + UI + clean

--- mrp_bom_sale_product_margin/models/mrp_bom.py
+++ mrp_bom_sale_product_margin/models/mrp_bom.py
@@ -10,8 +10,6 @@ from odoo.addons import decimal_precision as dp
 class MrpBom(models.Model):
     _inherit = "mrp.bom"
 
-    # TODO : warning dans les modules de base, pas forcement de product_id
-
     currency_id = fields.Many2one(related="product_tmpl_id.currency_id")
     # Fields related to cost
     product_standard_price = fields.Float(related="product_id.standard_price")
@@ -34,35 +32,6 @@ class MrpBom(models.Model):
     #
     # Other functions
     #
-
-    # COMMENTAIRE : Code si on veut tout gérer depuis le bom
-    # bom_lst_price = fields.Float(
-    # compute="_compute_lst_price", inverse="_inverse_lst_price",
-    # )
-    # Mais ça fait bcp de code, de doublon de fonctions et d'un point de vue UX/UI
-    # ça peut être perturbant de gérer des trucs de product depuis le BoM → risque de confusion
-    # @api.multi
-    # @api.depends("product_id", "product_id.lst_price")
-    # def _compute_lst_price(self):
-    #     for bom in self.filtered(lambda x: x.product_id):
-    #         bom.bom_lst_price = bom.product_id.lst_price
-    #
-    # @api.depends("lst_price")
-    # def _inverse_lst_price(self):
-    #     for bom in self.filtered(lambda x: x.product_id):
-    #         old_product_sale_price = bom.product_id.lst_price
-    #         bom.product_id.lst_price = bom.bom_lst_price
-    #         self.env.user.notify_success(
-    #             message=_(
-    #                 "Sale price is set to $%s (old price was $%s)"
-    #                 % (
-    #                     round(bom.product_id.lst_price, 2),
-    #                     round(old_product_sale_price, 2),
-    #                 )
-    #             ),
-    #             title=_("New Sale Price for %s" % bom.product_id.name),
-    #         )
-
     @api.multi
     @api.depends("product_id", "bom_line_ids")
     def _compute_standard_price_total(self):
--- mrp_bom_sale_product_margin/tests/__init__.py
+++ mrp_bom_sale_product_margin/tests/__init__.py
@@ -1,3 +1,1 @@
-from . import test_mrp_food_bom
-from . import test_product
-from . import test_seasonality
+from . import test_mrp_bom_sale_margin
