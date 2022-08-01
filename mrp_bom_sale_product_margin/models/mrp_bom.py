2b26e95c1a15d885c86a256751f10a638674726c
Quentin Dupont <perso@quentindupont.fr>
2022-08-01 11:02:12 +0200

    [ADD] Module mrp_bom_sale_product_margin

---
+++ mrp_bom_sale_product_margin/models/mrp_bom.py
@@ -0,0 +1,66 @@
+# Copyright (C) 2022 - Today: GRAP (http://www.grap.coop)
+# @author: Quentin DUPONT (quentin.dupont@grap.coop)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import _, api, fields, models
+
+from odoo.addons import decimal_precision as dp
+
+
+class MrpBom(models.Model):
+    _inherit = "mrp.bom"
+
+    currency_id = fields.Many2one(related="product_tmpl_id.currency_id")
+    # Fields related to cost
+    product_standard_price = fields.Float(related="product_id.standard_price")
+    standard_price_total = fields.Float(
+        string="BoM Cost",
+        track_visibility="onchange",
+        digits=dp.get_precision("Product Price"),
+        compute="_compute_standard_price_total",
+    )
+    diff_product_bom_standard_price = fields.Float(
+        digits=dp.get_precision("Product Price"),
+        compute="_compute_diff_product_bom_standard_price",
+    )
+
+    # Fields related to sale price
+    product_sale_price = fields.Float(related="product_id.lst_price")
+    product_margin_rate = fields.Float(related="product_id.standard_margin_rate")
+    product_margin_sale_text = fields.Char(compute="_compute_product_margin_sale_text")
+
+    #
+    # Other functions
+    #
+    @api.multi
+    @api.depends("product_id", "bom_line_ids")
+    def _compute_standard_price_total(self):
+        for bom in self:
+            bom.standard_price_total = sum(
+                x.standard_price_subtotal for x in bom.bom_line_ids
+            )
+
+    @api.multi
+    @api.depends("product_id.standard_price", "standard_price_total")
+    def _compute_diff_product_bom_standard_price(self):
+        for bom in self.filtered(lambda x: x.product_id):
+            bom.diff_product_bom_standard_price = (
+                bom.product_id.standard_price - bom.standard_price_total
+            )
+
+    # Functions to change product fields
+    @api.multi
+    def set_product_standard_price(self):
+        for bom in self.filtered(lambda x: x.product_id):
+            old_product_standard_price = bom.product_id.standard_price
+            bom.product_id.standard_price = bom.standard_price_total
+            self.env.user.notify_success(
+                message=_(
+                    "Cost price is set to $%s (former price was $%s)"
+                    % (
+                        round(bom.product_id.standard_price, 2),
+                        round(old_product_standard_price, 2),
+                    )
+                ),
+                title=_("New standard price for %s" % bom.product_id.name),
+            )
---
+++ mrp_bom_sale_product_margin/readme/DESCRIPTION.rst
@@ -0,0 +1,5 @@
+Help handling cost and sale price of product linked to bill of material.
+You can change the BoM and with one click, uppdate your standard_price and check
+the margin.
+
+ .. figure:: ../static/handle_cost_sale_price.gif
