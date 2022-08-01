fe8a2434da050b33917e90f0154e43bc8090a546
Quentin Dupont <perso@quentindupont.fr>
2022-08-01 10:56:39 +0200

    translation

--- mrp_bom_sale_product_margin/models/mrp_bom.py
+++ mrp_bom_sale_product_margin/models/mrp_bom.py
@@ -56,11 +56,11 @@ class MrpBom(models.Model):
             bom.product_id.standard_price = bom.standard_price_total
             self.env.user.notify_success(
                 message=_(
-                    "Cost price is set to $%s (old price was $%s)"
+                    "Cost price is set to $%s (former price was $%s)"
                     % (
                         round(bom.product_id.standard_price, 2),
                         round(old_product_standard_price, 2),
                     )
                 ),
-                title=_("New Standard Price for %s" % bom.product_id.name),
+                title=_("New standard price for %s" % bom.product_id.name),
             )
