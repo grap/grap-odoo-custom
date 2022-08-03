89f7387f58f3cd79fec8723a8e50b587072f1ec4
Quentin Dupont <perso@quentindupont.fr>
2022-08-03 13:17:39 +0200

    [IMP] keep trace of standard price, move from business to grap change views some stuff

--- grap_change_views_mrp/models/mrp_bom.py
+++ grap_change_views_mrp/models/mrp_bom.py
@@ -9,3 +9,13 @@ class MrpBom(models.Model):
     _inherit = "mrp.bom"
 
     product_name = fields.Char(related="product_id.name")
+
+    # override to make it related to product_id (and not product_tmpl_id)
+    # as it's related to product_id and required with this module
+    product_uom_id = fields.Many2one(
+        "uom.uom",
+        "Product Unit of Measure",
+        related="product_id.uom_id",
+        oldname="product_uom",
+        required=True,
+    )
