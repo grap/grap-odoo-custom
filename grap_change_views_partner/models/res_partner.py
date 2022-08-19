777403f49761074747c87b6f89bf7ef1d47df00e
Quentin Dupont <perso@quentindupont.fr>
2022-08-19 12:28:02 +0200

    [IMP] Inlive view with more fields, inclusive lang, one_line header, access button partner

--- grap_change_views_partner/models/res_partner.py
+++ grap_change_views_partner/models/res_partner.py
@@ -9,3 +9,15 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     property_account_position_id = fields.Many2one(track_visibility="always")
+
+    # Useful for accessing product in inline tree views
+    def see_current_partner(self):
+        self.ensure_one()
+        result = self.env.ref("base.action_partner_form").read()[0]
+        form_view = self.env.ref("base.view_partner_form")
+        result["views"] = [(form_view.id, "form")]
+        result["res_id"] = self.id
+        result["context"] = {
+            "form_view_initial_mode": "edit",
+        }
+        return result
