6350792d8316c0e90dc6c75462f0b936a68edf7e
Quentin Dupont <perso@quentindupont.fr>
2022-10-03 16:35:12 +0200

    [IMP] Supplier info view : not clickable name and direct button to product

--- grap_change_views_product/models/__init__.py
+++ grap_change_views_product/models/__init__.py
@@ -1,2 +1,3 @@
 from . import product_product
+from . import product_supplierinfo
 from . import product_template
---
+++ grap_change_views_product/models/product_supplierinfo.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2022-Today: GRAP (http://www.grap.coop)
+# @author: Quentin DUPONT (quentin.dupont@grap.coop)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models
+
+
+class SupplierInfo(models.Model):
+    _inherit = "product.supplierinfo"
+
+    def see_current_product_from_supplierinfo(self):
+        self.ensure_one()
+        result = self.env.ref(
+            "grap_change_views_product.action_product_product"
+        ).read()[0]
+        form_view = self.env.ref("grap_change_views_product.view_product_product_form")
+        result["views"] = [(form_view.id, "form")]
+        # Get product id → first product variant
+        product_template = self.env["product.template"].search(
+            [("id", "=", self.product_tmpl_id.id)]
+        )
+        product = product_template.product_variant_ids[0]
+        result["res_id"] = product.id
+        result["context"] = {
+            "form_view_initial_mode": "edit",
+        }
+        return result
