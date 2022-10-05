8975544c4708450ebf7f2dd4e586734f9296c35f
Quentin Dupont <perso@quentindupont.fr>
2022-10-05 12:04:57 +0200

    [FIX] bom uom is related to product

--- grap_change_views_mrp/models/mrp_bom.py
+++ grap_change_views_mrp/models/mrp_bom.py
@@ -9,12 +9,12 @@ class MrpBom(models.Model):
     _inherit = "mrp.bom"
 
     product_name = fields.Char(related="product_id.name")
-
+    #
     # override to make it related to product_id (and not product_tmpl_id)
     # as it's related to product_id and required with this module
-    # product_uom_id = fields.Many2one(
-    #     related="product_id.uom_id",
-    # )
+    product_uom_id = fields.Many2one(
+        related="product_id.uom_id",
+    )
 
     product_sale_ok = fields.Boolean(
         related="product_id.sale_ok",
