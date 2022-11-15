84b66a00fd731cfca0bdeadbae0436123f993b6e
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-11-15 16:39:25 +0100

    [REF] set grap_cooperative dev status to Alpha, due to 'immature' dependency

--- grap_cooperative/__manifest__.py
+++ grap_cooperative/__manifest__.py
@@ -7,6 +7,7 @@
     "name": "GRAP - Cooperative",
     "summary": "Add Directories, Companies, Colleges, Peoples, etc.",
     "version": "12.0.3.0.4",
+    "development_status": "Alpha",
     "category": "GRAP - Custom",
     "author": "GRAP",
     "website": "https://github.com/grap/grap-odoo-custom",
