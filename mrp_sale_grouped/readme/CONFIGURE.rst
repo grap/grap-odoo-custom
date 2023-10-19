4c014bbf750e82eebdcb05807027a16562daa617
Quentin Dupont <perso@quentindupont.fr>
2023-10-19 15:59:06 +0200

    [FIX] template id de merde [IMP] readme [ADD] tests

--- mrp_sale_grouped/readme/CONFIGURE.rst
+++ mrp_sale_grouped/readme/CONFIGURE.rst
@@ -1,12 +1,1 @@
-Quickly manage what you need to produce thanks to grouped sales
-
-.. figure:: ../static/description/mrp_sale_grouped.png
-
-1 : Create and name your production groupe
-2 : Associate sales to this production group
-3 : Retrieve sales and production order created thanks to this sales
-4 : Adjust quantities with Matrix2D view
-5 and 6 : Confirme sale orders one by one or all at once
-7 : Print PDF that sum up sales by product and sale
-
-.. figure:: ../static/description/report_production_plan.png
+* To use wizard production assistant, you need to create BoMs for your products
--- mrp_sale_grouped/tests/test_mrp_sale_grouped.py
+++ mrp_sale_grouped/tests/test_mrp_sale_grouped.py
@@ -9,12 +9,13 @@ class TestMrpSaleGrouped(TransactionCase):
     def setUp(self):
         super(TestMrpSaleGrouped, self).setUp()
         # Objects
-        mrp_sale_grouped_obj = self.env["mrp.sale.grouped"]
+        self.mrp_sale_grouped_obj = self.env["mrp.sale.grouped"]
+        self.wizard_obj = self.env["sale.grouped.wizard"]
         # Demo datas
         self.sale_order_gemini = self.env.ref("mrp_sale_grouped.demo_sale_gemini")
         self.sale_order_ready_mat = self.env.ref("mrp_sale_grouped.demo_sale_ready_mat")
         # Create one sale_grouped with sales
-        self.mrp_sale_grouped_1 = mrp_sale_grouped_obj.create(
+        self.mrp_sale_grouped_1 = self.mrp_sale_grouped_obj.create(
             {"name": "TEST Grouped Sale"}
         )
         # Add grouped to sales
@@ -27,6 +28,21 @@ class TestMrpSaleGrouped(TransactionCase):
     def test_02_grouped_sales_check_confirm_quotations(self):
         self.assertEqual(self.mrp_sale_grouped_1.order_ids[0].state, "draft")
         self.assertEqual(self.mrp_sale_grouped_1.order_ids[1].state, "draft")
+        self.assertEqual(self.mrp_sale_grouped_1.sales_state, "sales_in_progress")
         self.mrp_sale_grouped_1.confirm_all_sale_order()
         self.assertEqual(self.mrp_sale_grouped_1.order_ids[0].state, "sale")
         self.assertEqual(self.mrp_sale_grouped_1.order_ids[1].state, "sale")
+        self.mrp_sale_grouped_1._compute_sales_state()
+        self.assertEqual(self.mrp_sale_grouped_1.sales_state, "all_sales_confirmed")
+
+    def test_03_check_mrp_grouped_production_state(self):
+        self.assertEqual(
+            self.mrp_sale_grouped_1.production_state, "all_production_done"
+        )
+
+    def test_04_sale_grouped_report(self):
+        # Launch wizard and report action
+        wizard = self.wizard_obj.with_context(
+            active_ids=[self.mrp_sale_grouped_1.id]
+        ).create({})
+        wizard.print_sale_sum_up()
