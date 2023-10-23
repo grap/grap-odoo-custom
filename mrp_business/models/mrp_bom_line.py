443c2e1866be0ba1f76522f8c6a2967816ce9034
Quentin Dupont <perso@quentindupont.fr>
2023-10-23 11:57:07 +0200

    [IMP] Weight based on net qty, i18n, field order on bom_line

--- mrp_business/models/mrp_bom_line.py
+++ mrp_business/models/mrp_bom_line.py
@@ -21,15 +21,17 @@ class MrpBomLine(models.Model):
         digits=(16, 3),
     )
 
-    @api.depends("product_qty", "product_uom_id", "product_id", "product_id.net_weight")
+    @api.depends(
+        "product_qty_net", "product_uom_id", "product_id", "product_id.net_weight"
+    )
     def _compute_line_weight(self):
         for line in self:
             if line.product_uom_id.category_id.measure_type == "weight":
-                line.line_weight = line.product_qty / line.product_uom_id.factor
+                line.line_weight = line.product_qty_net / line.product_uom_id.factor
             else:
-                line.line_weight = line.product_id.net_weight * line.product_qty
+                line.line_weight = line.product_id.net_weight * line.product_qty_net
 
-    @api.depends("product_qty", "bom_id.bom_line_ids.product_qty")
+    @api.depends("product_qty_net", "bom_id.bom_line_ids.product_qty_net")
     def _compute_line_weight_percentage(self):
         for line in self:
             bom_total_weight = line.bom_id.bom_components_total_weight
