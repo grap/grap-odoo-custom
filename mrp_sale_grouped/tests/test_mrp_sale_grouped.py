eb08be334665367a8ea76b16ee47ce02e5a03019
Quentin Dupont <perso@quentindupont.fr>
2023-10-24 13:52:48 +0200

    [ADD] tests and demos

--- mrp_sale_grouped/tests/test_mrp_sale_grouped.py
+++ mrp_sale_grouped/tests/test_mrp_sale_grouped.py
@@ -2,6 +2,7 @@
 # @author: Quentin DUPONT (quentin.dupont@grap.coop)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+from odoo.exceptions import Warning as UserError
 from odoo.tests.common import TransactionCase
 
 
@@ -11,16 +12,28 @@ class TestMrpSaleGrouped(TransactionCase):
         # Objects
         self.mrp_sale_grouped_obj = self.env["mrp.sale.grouped"]
         self.wizard_obj = self.env["sale.grouped.wizard"]
+        self.wizard_purchase_list_obj = self.env["bom.print.purchase.list.wizard"]
         # Demo datas
         self.sale_order_gemini = self.env.ref("mrp_sale_grouped.demo_sale_gemini")
         self.sale_order_ready_mat = self.env.ref("mrp_sale_grouped.demo_sale_ready_mat")
+        self.sale_order_with_boms = self.env.ref(
+            "mrp_sale_grouped.demo_sale_deco_addict"
+        )
+        self.bom_pie = self.env.ref("mrp_sale_grouped.demo_bom_tomato_tart")
+        self.product_tmpl_pie = self.env.ref(
+            "mrp_sale_grouped.demo_product_template_tomato_tart"
+        )
         # Create one sale_grouped with sales
         self.mrp_sale_grouped_1 = self.mrp_sale_grouped_obj.create(
             {"name": "TEST Grouped Sale"}
         )
+        self.mrp_sale_grouped_with_boms = self.mrp_sale_grouped_obj.create(
+            {"name": "TEST with BoMs"}
+        )
         # Add grouped to sales
         self.sale_order_gemini.mrp_sale_grouped_id = self.mrp_sale_grouped_1
         self.sale_order_ready_mat.mrp_sale_grouped_id = self.mrp_sale_grouped_1
+        self.sale_order_with_boms.mrp_sale_grouped_id = self.mrp_sale_grouped_with_boms
 
     def test_01_create_grouped_check_sale_linked(self):
         self.assertEqual(self.mrp_sale_grouped_1.orders_qty, 2)
@@ -46,3 +59,15 @@ class TestMrpSaleGrouped(TransactionCase):
             active_ids=[self.mrp_sale_grouped_1.id]
         ).create({})
         wizard.print_sale_sum_up()
+
+    def test_05_check_wizard_production_with_boms(self):
+        self.wizard_purchase_list_obj.with_context(
+            active_ids=[self.mrp_sale_grouped_with_boms.id],
+            active_model="mrp.sale.grouped",
+        ).create({})
+
+    def test_06_check_wizard_production_without_boms(self):
+        with self.assertRaises(UserError):
+            self.wizard_purchase_list_obj.with_context(
+                active_ids=[self.mrp_sale_grouped_1.id], active_model="mrp.sale.grouped"
+            ).create({})
