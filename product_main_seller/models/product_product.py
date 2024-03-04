21312e5c84c623ff1c9785c8465d66f6d2981ec0
Quentin Dupont <perso@quentindupont.fr>
2024-03-04 16:47:51 +0100

    [FIX] main_seller - adjust depends and temp function

--- product_main_seller/models/product_product.py
+++ product_main_seller/models/product_product.py
@@ -8,6 +8,10 @@ from odoo import api, fields, models
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
+    diff_product_main_first_seller = fields.Boolean(
+        compute="_compute_diff_product_main_first_seller",
+    )
+
     product_main_seller_partner_id = fields.Many2one(
         comodel_name="res.partner",
         string="Main Product Seller Partner",
@@ -17,8 +21,31 @@ class ProductProduct(models.Model):
     )
 
     @api.multi
-    @api.depends("variant_seller_ids.sequence")
+    @api.depends("variant_seller_ids.sequence", "variant_seller_ids.name")
     def _compute_main_seller_partner_id(self):
         for prod in self:
             if len(prod.variant_seller_ids):
                 prod.product_main_seller_partner_id = prod.variant_seller_ids[0].name
+
+    # TEMP : In some case, main seller was not well calculated, and user
+    # needs to set it by hand
+    @api.multi
+    def set_main_seller(self):
+        self._compute_main_seller_partner_id()
+
+    @api.multi
+    @api.depends(
+        "variant_seller_ids.sequence",
+        "variant_seller_ids.name",
+        "product_main_seller_partner_id",
+    )
+    def _compute_diff_product_main_first_seller(self):
+        for prod in self:
+            if len(prod.variant_seller_ids):
+                if (
+                    prod.product_main_seller_partner_id
+                    != prod.variant_seller_ids[0].name
+                ):
+                    prod.diff_product_main_first_seller = True
+                else:
+                    prod.diff_product_main_first_seller = False
