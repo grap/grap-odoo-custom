e899590df648b327c051a8f97abec54275399f46
Quentin Dupont <quentin.dupont@grap.coop>
2024-11-21 12:04:49 +0100

    [16.0][IMP] Merging page in mrp_bom form view, adding custom fields

---
+++ fermente_mrp_bom_form_view/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright (C) 2024 - Today: GRAP (http://www.grap.coop)
+# @author: Quentin DUPONT (quentin.dupont@grap.coop)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Fermente - MRP BoM Form View",
+    "version": "16.0.1.0.0",
+    "category": "Web",
+    "author": "GRAP",
+    "website": "https://github.com/grap/grap-odoo-custom",
+    "license": "AGPL-3",
+    "depends": [
+        "mrp",
+        # OCA
+        "web_widget_numeric_step",
+        "mrp_bom_widget_section_and_note_one2many",
+        # GRAP
+        "mrp_bom_tag",
+        "mrp_bom_line_net_qty",
+        "mrp_bom_product_price_margin",
+    ],
+    "data": [
+        "views/view_mrp_bom.xml",
+    ],
+}
