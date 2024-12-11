34d340aae5983fd55185644390bcfb0094a6e35a
Quentin Dupont <quentin.dupont@grap.coop>
2024-12-11 10:09:43 +0100

    rename module and merge mrp_bom_print and mrp_business meal category

---
+++ mrp_bom_report_allergen/models/mrp_bom.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2022 - Today: GRAP (http://www.grap.coop)
+# @author: Quentin DUPONT (quentin.dupont@grap.coop)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class MrpBom(models.Model):
+    _inherit = "mrp.bom"
+
+    # Column Section
+    meal_category_id = fields.Many2one(
+        related="product_tmpl_id.meal_category_id",
+        string="Meal category",
+    )
--- mrp_bom_report_allergen/readme/ROADMAP.rst
+++ mrp_bom_report_allergen/readme/ROADMAP.rst
@@ -1,1 +1,3 @@
+TODO : Continue migration in 16.0. Clean code etc.
+
 Allergens table works only when using product product on Bill Of Material.
