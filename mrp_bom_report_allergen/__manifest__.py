b90a72b69bcd94b12b122449b2a70bd7c80d19ca
Quentin Dupont <quentin.dupont@grap.coop>
2025-01-15 09:45:33 +0100

    [IMP] Readme, remove duplicate code and deprecated demo

--- mrp_bom_report_allergen/__manifest__.py
+++ mrp_bom_report_allergen/__manifest__.py
@@ -21,7 +21,6 @@
         "data/report_paperformat.xml",
         "report/report_bom_allergens.xml",
         "report/ir_actions_report.xml",
-        "views/view_mrp_bom.xml",
         "views/view_mrp_meal_category.xml",
         "views/view_product_template.xml",
         "wizard/view_bom_print_wizard.xml",
