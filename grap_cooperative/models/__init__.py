504cf854219f1b88111462e050a221c91a222980
Sylvain LE GAL <sylvain.legal@grap.coop>
2025-02-06 17:35:18 +0100

    [REM] remove college, mandate, pronouns, etc...

--- grap_cooperative/models/__init__.py
+++ grap_cooperative/models/__init__.py
@@ -1,4 +1,2 @@
-from . import grap_college
-from . import grap_mandate
 from . import grap_people
 from . import res_company
--- grap_cooperative/models/grap_people.py
+++ grap_cooperative/models/grap_people.py
@@ -4,52 +4,40 @@
 
 from datetime import datetime
 
-from odoo import api, fields, models, tools
+from odoo import api, fields, models
 
 
 class GrapPeople(models.Model):
     _name = "grap.people"
     _description = "GRAP Peoples"
 
-    _order = "name"
+    _order = "first_name, last_name"
 
-    # Columns section
-    name = fields.Char(string="Full name", required=True, store=True)
+    first_name = fields.Char(required=True)
 
-    first_name = fields.Char(string="First name", required=True)
+    last_name = fields.Char(required=True)
 
-    last_name = fields.Char(string="Last name", required=True)
+    image = fields.Binary(attachment=True)
 
-    pronouns = fields.Char(string="Pronoun(s)")
+    image_medium = fields.Binary(attachment=True)
 
-    image = fields.Binary(string="Image", attachment=True)
+    image_small = fields.Binary(attachment=True)
 
-    image_medium = fields.Binary(string="Medium-sized image", attachment=True)
+    street = fields.Char()
 
-    image_small = fields.Binary(string="Small-sized image", attachment=True)
+    zip = fields.Char()
 
-    street = fields.Char(string="Street")
+    city = fields.Char()
 
-    zip = fields.Char(string="Zip")
+    working_email = fields.Char()
 
-    city = fields.Char(string="City")
+    working_phone = fields.Char()
 
-    profession = fields.Char(string="Profession")
+    birthdate = fields.Date()
 
-    working_email = fields.Char(string="Working Email")
-
-    working_phone = fields.Char(string="Working Phone")
-
-    birthdate = fields.Date("Birthdate")
-
-    is_birthday = fields.Boolean(string="Is Birthday", compute="_compute_is_birthday")
-
-    private_phone = fields.Char(string="Private Phone")
-
-    college_id = fields.Many2one(string="College", comodel_name="grap.college")
+    private_phone = fields.Char()
 
     company_id = fields.Many2one(
-        string="Company",
         comodel_name="res.company",
         domain="[('is_displayed_in_directory', '=', True)]",
         context={
@@ -59,55 +47,3 @@ class GrapPeople(models.Model):
     )
 
     company_code = fields.Char(related="company_id.code", store=True)
-
-    company_manager_ids = fields.Many2many(
-        string="Companies with mandates",
-        comodel_name="res.company",
-        relation="grap_people_companies_managers_rel",
-        column1="people_id",
-        column2="company_manager_id",
-    )
-
-    mandate_ids = fields.Many2many(
-        string="Cooperative mandates",
-        comodel_name="grap.mandate",
-        relation="grap_people_mandate_rel",
-        column1="people_id",
-        column2="mandate_id",
-    )
-
-    @api.depends("birthdate")
-    def _compute_is_birthday(self):
-        for people in self.filtered(lambda x: x.birthdate):
-            now = datetime.now().date()
-            birthdate = people.birthdate.replace(year=now.year)
-            delta = birthdate - now
-            if delta.days >= -1 and delta.days < 2:
-                people.is_birthday = True
-            else:
-                people.is_birthday = False
-        for people in self.filtered(lambda x: not x.birthdate):
-            people.is_birthday = False
-
-    # Set people name with first and last name
-    # Optimize for creating people directly from company form
-    @api.onchange("first_name", "last_name")
-    def _handle_name(self):
-        for people in self:
-            if people.name and not people.first_name and not people.last_name:
-                split_name = people.name.split()
-                if len(split_name) > 0:
-                    people.first_name = split_name[0]
-                if len(split_name) > 1:
-                    people.last_name = " ".join(split_name[1:])
-            else:
-                people.name = ""
-                if people.last_name:
-                    people.last_name = people.last_name.upper()
-                    people.name = people.last_name
-                if people.first_name:
-                    people.first_name = people.first_name.capitalize()
-                    people.name += " " + people.first_name
-
-    def detach_people_from_company(self):
-        self.write({"company_id": False})
