43653d468a65fa48100636f8b4df935c52df49bd
Sylvain LE GAL <sylvain.legal@grap.coop>
2025-02-11 15:40:31 +0100

    [ADD] fermente_base module

---
+++ fermente_base/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2024 - Today: Sylvain LE GAL (http://www.grap.coop)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Fermente - Base",
+    "version": "16.0.1.0.0",
+    "category": "Web",
+    "author": "GRAP",
+    "website": "https://github.com/grap/grap-odoo-custom",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "data": [
+        "views/view_res_partner.xml",
+    ],
+}
