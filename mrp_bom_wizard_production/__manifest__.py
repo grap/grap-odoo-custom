c0edd4b2124ab0b72df31b7447928b78bf5ac8cb
Quentin Dupont <quentin.dupont@grap.coop>
2025-02-13 15:13:33 +0100

    Change version

--- mrp_bom_wizard_production/__manifest__.py
+++ mrp_bom_wizard_production/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "MRP BoM Wizard production",
     "summary": "Wizard linked to Bill of Materials to help your production.",
-    "version": "16.0.1.1.0",
+    "version": "16.0.1.2.0",
     "category": "Manufacturing",
     "author": "GRAP",
     "website": "https://github.com/grap/grap-odoo-custom",
