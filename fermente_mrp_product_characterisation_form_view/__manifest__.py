31c60185015f21b726df2c83c5ffa68c2c7f8904
Quentin Dupont <quentin.dupont@grap.coop>
2025-02-14 15:41:39 +0100

    [16.0][ADD] fermente_mrp_product_characterisation_form_view

---
+++ fermente_mrp_product_characterisation_form_view/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2025 - Today: GRAP (http://www.grap.coop)
+# @author: Quentin DUPONT (quentin.dupont@grap.coop)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Fermente - Product Characterisation Product Form View",
+    "version": "16.0.1.0.0",
+    "category": "Custom",
+    "author": "GRAP",
+    "website": "https://github.com/grap/grap-odoo-custom",
+    "license": "AGPL-3",
+    "depends": ["mrp_product_characterisation"],
+    "data": [
+        "views/view_product_template.xml",
+    ],
+}
---
+++ fermente_mrp_product_characterisation_form_view/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+Customize Odoo / odoo / ``product`` form view changed by mrp_product_characterisation.
+
+* Hide field is_component and is_intermediate (information can be guessed with smart buttons)
