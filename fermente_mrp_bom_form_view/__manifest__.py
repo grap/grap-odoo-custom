19235d22cc2075429047e626757f0d96e37d7107
Quentin Dupont <quentin.dupont@grap.coop>
2025-02-20 15:50:13 +0100

    [IMP] fermente_mrp_bom_form-polish-view

--- fermente_mrp_bom_form_view/__manifest__.py
+++ fermente_mrp_bom_form_view/__manifest__.py
@@ -18,7 +18,9 @@
         # GRAP
         "mrp_bom_tag",
         "mrp_bom_line_net_qty",
+        "mrp_bom_product_allergen",
         "mrp_bom_product_price_margin",
+        "product_label_mrp",
     ],
     "data": [
         "views/view_mrp_bom.xml",
