a4be5a391029cfaf236195378ef205a377e1ec99
Sylvain LE GAL <sylvain.legal@grap.coop>
2025-02-28 19:24:03 +0100

    [MIG] grap_theme: Migration to 16.0 (from 12.0)

--- grap_theme/__manifest__.py
+++ grap_theme/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "GRAP - Web Theme",
     "summary": "Customize Odoo web User Interface",
-    "version": "12.0.1.3.5",
+    "version": "16.0.1.0.0",
     "category": "GRAP Custom",
     "author": "GRAP",
     "website": "https://github.com/grap/grap-odoo-custom",
