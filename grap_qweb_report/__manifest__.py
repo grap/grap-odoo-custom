6f59d097408644748058e9be76170a9da0816299
Quentin Dupont <quentin.dupont@grap.coop>
2025-09-12 14:23:30 +0200

    [MIG] grap_qweb_report: Migration to 16.0 (from 12.0)

--- grap_qweb_report/__manifest__.py
+++ grap_qweb_report/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "GRAP - Custom Qweb Reports",
-    "version": "12.0.2.2.0",
+    "version": "16.0.1.0.0",
     "category": "GRAP - Custom",
     "author": "GRAP",
     "website": "https://github.com/grap/grap-odoo-custom",
