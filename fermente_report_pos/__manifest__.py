cd49b844a6bcd7998036a560ead4b4f6f8a61ebf
Quentin Dupont <quentin.dupont@grap.coop>
2025-09-26 17:40:13 +0200

    [REF][MIG] fermente_report_pos

--- fermente_report_pos/__manifest__.py
+++ fermente_report_pos/__manifest__.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2018 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
-# @author: Quentin DUPONT (https://twitter.com/pondupont)
+# @author: Quentin DUPONT
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
@@ -10,8 +10,12 @@
     "author": "GRAP",
     "website": "https://github.com/grap/grap-odoo-custom",
     "license": "AGPL-3",
-    "depends": [
-        "point_of_sale",
-    ],
-    "installable": True,
+    "depends": ["point_of_sale"],
+    "assets": {
+        "point_of_sale.assets": [
+            "fermente_report_pos/static/src/js/OrderReceipt.js",
+            "fermente_report_pos/static/src/xml/OrderReceipt.xml",
+        ],
+    },
+    "installable": True,    
 }
---
+++ fermente_report_pos/static/src/js/OrderReceipt.js
@@ -0,0 +1,33 @@
+/*
+Copyright (C) 2018-Today GRAP (http://www.grap.coop)
+@author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+*/
+
+odoo.define('fermente_report_pos.OrderReceipt', function (require) {
+    'use strict';
+
+    const {Order} = require("point_of_sale.models");
+    const Registries = require("point_of_sale.Registries");
+
+    var core = require("web.core");
+    var _t = core._t;
+
+    const OverloadOrder = (OriginalOrder) =>
+        class extends OriginalOrder {
+            
+            export_for_printing() {
+                var receipt = super.export_for_printing(...arguments);
+                receipt.pricelist_id = this.pricelist.id;
+                receipt.pricelist_name = this.pricelist.name;
+                receipt.pricelist_default =
+                    receipt.pricelist_id === this.pos.config.pricelist_id[0];
+                return receipt;
+            }
+        };
+
+    Registries.Model.extend(Order, OverloadOrder);
+
+    return Order;
+
+});
