8355c2c85795255fa09f5bc92020078a717bd05b
Quentin Dupont <quentin.dupont@grap.coop>
2025-10-21 14:10:57 +0200

    [16.0][ADD] fermente_l10n_fr_siret

---
+++ fermente_l10n_fr_siret/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2025 - Today: Quentin DUPONT (http://www.grap.coop)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Fermente - L10N FR Siret",
+    "version": "16.0.1.0.0",
+    "category": "Custom",
+    "author": "GRAP",
+    "website": "https://github.com/grap/grap-odoo-custom",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_fr_siret",
+    ],
+    "data": [
+        "views/view_res_partner.xml",
+    ],
+}
