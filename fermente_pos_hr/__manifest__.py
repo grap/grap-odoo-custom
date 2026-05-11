3bd9130eedce2e9f91316cc4b58178c13dd66672
Sylvain LE GAL <sylvain.legal@grap.coop>
2026-05-11 22:59:32 +0200

    [IMP] fermente_pos: hide some fields in pos.order tree view to make the view lighter

---
+++ fermente_pos_hr/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright (C) 2024 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Fermente - Point Of Sale HR",
+    "version": "16.0.1.0.0",
+    "category": "Web",
+    "author": "GRAP",
+    "website": "https://github.com/grap/grap-odoo-custom",
+    "license": "AGPL-3",
+    "depends": ["pos_hr"],
+    "data": ["views/view_pos_order.xml"],
+}
