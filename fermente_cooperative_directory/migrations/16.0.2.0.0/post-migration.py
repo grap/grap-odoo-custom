ed2501f951d6c579a970be0da5382e92ee6ddb46
Sylvain LE GAL <sylvain.legal@grap.coop>
2026-05-15 00:22:05 +0200

    fixup! [FIX] fermente_cooperative_directory : try to fix migration of images

--- fermente_cooperative_directory/migrations/16.0.2.0.0/post-migration.py
+++ fermente_cooperative_directory/migrations/16.0.2.0.0/post-migration.py
@@ -27,6 +27,7 @@ def _create_hr_employee_from_grap_people(env):
         vals = {
             "firstname": row["first_name"],
             "lastname": row["last_name"],
+            "name": env["hr.employee"]._get_name(row["last_name"], row["first_name"]),
             "work_email": row["working_email"],
             "mobile_phone": row["working_phone"],
             "company_id": row["company_id"],
@@ -49,6 +50,15 @@ def _create_hr_employee_from_grap_people(env):
             )
             if len(user) == 1:
                 vals["user_id"] = user.id
+                if "[" in user.name or "]" in user.name:
+                    _logger.info(f"Fixing user name {user.name}")
+                    user.name = user.name.replace("[", "").replace("]", "")
+                if not user.firstname or not user.lastname:
+                    _logger.info(
+                        f"Deducing first name / last name from {user.name} ..."
+                    )
+                    user.write(env["res.partner"]._get_inverse_name(user.name))
+
             elif len(user) > 1:
                 _logger.warning(f"Many users found for email {row['working_email']}...")
 
@@ -73,13 +83,19 @@ def _create_hr_employee_from_grap_people(env):
             )
 
             openupgrade.logged_query(
+                env.cr,
                 """UPDATE ir_attachment
-                set res_id = %s
-                res_model = 'hr.employee'
+                SET res_id = %s,
+                res_model = 'hr.employee',
                 res_field = 'image_1920'
+                WHERE id = %s
                 """,
-                (employee.id,),
+                (
+                    employee.id,
+                    attachment.id,
+                ),
             )
+            env["hr.employee"].invalidate_cache()
             employee.image_1920 = employee.image_1920
 
 
