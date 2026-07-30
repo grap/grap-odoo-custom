cab7b0c4e775175a06feb82f60c613194da8b723
Sylvain LE GAL <sylvain.legal@grap.coop>
2026-07-30 17:26:46 +0200

    [ADD] fermente_spreadsheet_dashboard: module to hide dashboard > configuration > dashboard entry menu for non admin user

---
+++ fermente_spreadsheet_dashboard/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright (C) 2026 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Fermente - Spreadsheet Dashboard",
+    "version": "16.0.1.0.0",
+    "category": "Custom",
+    "author": "GRAP",
+    "website": "https://github.com/grap/grap-odoo-custom",
+    "license": "AGPL-3",
+    "depends": ["spreadsheet_dashboard"],
+    "data": ["views/menu.xml"],
+}
