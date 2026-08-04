abc2c1ed7f9a212edb5880a95e8d03d89950c7ec
Sylvain LE GAL <sylvain.legal@grap.coop>
2026-08-04 16:12:17 +0200

    [FIX] fermente_product_category_active: avoid to prevent installing pos_loyalty module (for exemple)

--- fermente_product_category_active/models/product_template.py
+++ fermente_product_category_active/models/product_template.py
@@ -2,15 +2,16 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models, tools
+from odoo import fields, models
 
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    categ_id = fields.Many2one(default=lambda x: x._get_default_category_id())
+    categ_id = fields.Many2one(default=lambda x: x._default_category_id())
 
-    @tools.ormcache()
-    def _get_default_category_id(self):
+    def _default_category_id(self):
         result = super()._get_default_category_id()
+        if self.env.context.get("install_mode"):
+            return result
         return result.active and result or False
